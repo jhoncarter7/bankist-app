bc64e629a00fe1885dd353e7ec151be1f91b0172
jhoncarter7 <90902071+jhoncarter7@users.noreply.github.com>
2022-08-03 16:51:17 +0530

    update

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 
- 
- const account1 = {
-        owner: 'Nikhil rai',
-        movements: [200, 600, -300, 1000, -500, 100, 200, 600, -300, 1000, -500, 100,  200, 600, -300, 1000, -500, 100],
-        pin: 1111,
-        interest: 2  /* % */
 
-    }
- const account2 = {
-        owner: 'Aditya singh',
-        movements: [3000, 6000, -1500, 10000, -5000, 150],
-        pin: 2222,
-        interest: 2.5  /* % */
+const account1 = {
+    owner: 'Nikhil rai',
+    movements: [200, 600, -300, 1000, -500, 100, 200, 600, -300, 1000, 100, 200, 600, -300, 1000, -500, 100],
+    pin: 1111,
+    interest: 2  /* % */
 
-    }
- const account3 = {
-        owner: 'modi ji',
-        movements: [400, 7000, -2000, 300, -500, 900],
-        pin: 3333,
-        interest: 1.2  /* % */
+}
+const account2 = {
+    owner: 'Aditya singh',
+    movements: [3000, 6000, -1500, 10000, -5000, 150],
+    pin: 2222,
+    interest: 2.5  /* % */
 
-    }
+}
+const account3 = {
+    owner: 'modi ji',
+    movements: [400, 7000, -2000, 300, -500, 900],
+    pin: 3333,
+    interest: 1.2  /* % */
+
+}
 
 
 
@@ -45,13 +45,13 @@ const depositeDate = document.querySelector('.deposite-date');
 const userDepositeMoney = document.querySelector('.user-deposite-money');
 const depositing = document.querySelector('.in');
 const totalIn = document.querySelector('.total-in');
-const withdrawOut = document.querySelector('.out');
+const totalOut = document.querySelector('.total-out');
 const interest = document.querySelector('.interest');
 const interestMoney = document.querySelector('.interest-money');
 const transfermoneyuserName = document.querySelector('.transfer-money-userName');
-const transferMoneyPass = document.querySelector('.transfer-money-pass');
+const transferMoneyAmount = document.querySelector('.transfer-money-amount');
 const transferBtn = document.querySelector('.transfer-btn');
-const loanNserName = document.querySelector('.loan-userName');
+const loanAmount = document.querySelector('.loan-amount');
 const loanBtn = document.querySelector('.loan-btn');
 const closeAccountUserName = document.querySelector('.close-account-userName');
 const closeAccountPass = document.querySelector('.close-account-pass');
@@ -62,29 +62,67 @@ const loggedoutTimer = document.querySelector('.loggedout-timer');
 
 /////////////////////////////////////////////////////////////////////////
 
-const displaySummary = function (account1) {
+const displaySummary = function (currentAccount) {
     containerUserDetails.innerHTML = '';
 
-    account1.movements.forEach(function(mov, i){ 
+    currentAccount.movements.forEach(function (mov, i) {
         const options = mov > 0 ? 'deposits' : 'withdraws'
-    const html = 
-           ` <div class="user-data">
+        const html =
+            ` <div class="user-data">
            <p class="user-type user-${options}">${i + 1} ${options} </p>
            <p class="deposite-date">27/07/20022</p>
-           <p class="user-deposite-money">${mov}</p>
+           <p class="user-deposite-money">Rs ${mov}</p>
            </div>
         `;
         containerUserDetails.insertAdjacentHTML('afterbegin', html)
- } )
-    
+
+    })
+
 }
-displaySummary(account1)
 
 
 
 
 
-   
+//    Real balance in account
+const displayBlance = function (acc) {
+
+    acc.balance = currentAccount.movements
+        .reduce((curr, mov) => curr + mov)
+
+    totalBalance.innerHTML = `Rs: ${acc.balance}`;
+
+}
+
+
+
+
+const dipoWithdrawIntr = function (acc) {
+    const deposit = acc.movements
+        .filter(mov => mov > 0)
+        .reduce((curr, mov) => curr + mov, 0);
+
+    totalIn.innerHTML = `${deposit} `
+
+
+
+    const withdraw = acc.movements.filter(mov => mov < 0)
+        .reduce((curr, mov) => curr + mov, 0)
+    totalOut.innerHTML = `${withdraw} `
+
+    totalOut.innerHTML = `${withdraw}`
+    const interestValue = acc.interest;
+    const interest = acc.movements
+        .map(mov => (mov * interestValue)/100)
+        .filter(mov => mov > 0)
+        .reduce((curr, mov) => curr + mov)
+console.log(interest)
+    interestMoney.innerHTML = `Rs: ${(interest)}`
+}
+
+
+
+
 
 
 
@@ -100,17 +138,25 @@ displaySummary(account1)
 
 
 // username and pin created
-const creatUserName = function(accs){
+const creatUserName = function (accs) {
     accs.forEach(acc => {
         acc.userName = acc.owner
-        .toLowerCase()
-        .split(' ')
-        .map((name) =>name[0])
-        .join('')
+            .toLowerCase()
+            .split(' ')
+            .map((name) => name[0])
+            .join('')
     });
-    console.log(accs)
+
 }
 creatUserName(accounts);
+
+
+const updateUI = function (currentAccount) {
+    displaySummary(currentAccount)
+    displayBlance(currentAccount)
+    dipoWithdrawIntr(currentAccount)
+
+}
 
 
 
@@ -118,15 +164,97 @@ creatUserName(accounts);
 let currentAccount;
 
 // login btn of nav
-loginbtn.addEventListener('click', function(e){
+loginbtn.addEventListener('click', function (e) {
     e.preventDefault();
- 
+
     currentAccount = accounts.find(acc =>
         acc.userName === userNames.value
     );
- console.log(currentAccount);
- if(currentAccount.pin == userPass.value){
-    hide.style.opacity = 100;
-    
- }
+    console.log(currentAccount);
+    if (currentAccount.pin == userPass.value) {
+        hide.style.opacity = 100;
+        balanceHeading.innerHTML = currentAccount.owner + ",  your current balance =>"
+        userPass.value = userNames.value = '';
+        updateUI(currentAccount)
+    }
+
+})
+
+
+
+
+loanBtn.addEventListener('click', function (e) {
+    e.preventDefault()
+
+    const loanMoney = Number(loanAmount.value);
+
+    loanAmount.value = '';
+
+    If(loanMoney > 0)
+    {
+
+        currentAccount.movements.push(loanMoney);
+    }
+    updateUI(currentAccount)
+
+}
+)
+
+
+
+
+
+
+
+
+
+// Transfer Money
+transferBtn.addEventListener('click', function (e) {
+    e.preventDefault()
+    const reciverAccount = accounts.find(acc => acc.userName == transfermoneyuserName.value);
+
+    const money = Number(transferMoneyAmount.value);
+    transfermoneyuserName.value = transferMoneyAmount.value = '';
+
+
+
+    if (money > 0 &&
+        reciverAccount?.userName !== currentAccount.userName &&
+        currentAccount.balance >= money
+    ) {
+
+        reciverAccount.movements.push(money)
+        currentAccount.movements.push(-money)
+        updateUI(currentAccount)
+    }
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+// close account 
+closeAccountBtn.addEventListener('click', function (e) {
+    e.preventDefault();
+
+
+
+    if (currentAccount.userName === closeAccountUserName.value &&
+        currentAccount.pin === Number(closeAccountPass.value)) {
+        // console.log("ok")
+
+        const index = accounts.findIndex(acc => acc.userName === currentAccount.userName)
+        accounts.splice(index, 1)
+        hide.style.opacity = 0;
+    }
+    closeAccountUserName.value = closeAccountPass.value = '';
+
 })
